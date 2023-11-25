1149edacd4263879092aa22fe874bd75aecb7e6f
Ankitakamthe153 <88307915+Ankitakamthe153@users.noreply.github.com>
2023-11-25 21:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function promiseFunction() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+promiseFunction().then((result) => {
+  document.getElementById("output").innerHTML = result;
+});
+
